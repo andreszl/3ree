db7fa20ecbf8397dd2d96dafc11baff40767e5a3
AndresZL <andreszl.devcode@gmail.com>
2018-05-17 09:17:54 -0500

    redux implementation small changes in the reducer - src/reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,11 +7,18 @@ let user = {
 export default (state = user, action ) => {
 	switch(action.type){
         case SET_USER_ID:
-            return Object.assign({}, state, {
-                userId: action.userId
-            });
-            user = [...state,user(action)]
-			console.log('game as state in store', user)
+            // Antes ; retornaba el objecto sin imprimirlo
+
+            // return Object.assign({}, state, {
+            //     userId: action.userId
+            // });
+
+            // Despues  imprimo el objecto antes de retornarlo
+            const { userId } = action
+            user = {
+                userId
+            }
+			console.log('userId as state in store', user)
 			return user
 		default: 
 		 return state
